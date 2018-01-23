ce483f7f6ae2ff9e205397a35ed2c0c2e216030a
Danny Claus <dclaus@doubleslash.org>
2018-01-23 09:14:14 +0100

    minor refectoring

--- src/main/java/de/doubleslash/ml/techdays/ClassificationExample.java
+++ src/main/java/de/doubleslash/ml/techdays/ClassificationExample.java
@@ -9,25 +9,48 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class ClassificationExample {
+	private static SparkSession spark;
+	private static final double SQM_MUC = 73;
+	private static final double PRICE_MUC = 1000;
+
+	private static final double SQM_FN = 65;
+	private static final double PRICE_FN = 700;
+
 	public static void main(String[] args) {
-		SparkSession spark = SparkSession.builder().appName("JavaLinearSVCExample").config("spark.master", "local")
-				.getOrCreate();
-		spark.sparkContext().setLogLevel("ERROR");
+		spark = initSparkSession();
 
 		// Load training data
 		Dataset<Row> training = spark.read().format("libsvm").load("data/mietkosten_fn_muc.txt");
 
-		LinearSVC lsvc = new LinearSVC().setMaxIter(10).setRegParam(0.1);
+		LinearSVCModel lsvcModel = getSVCModel(training);
 
+		predictModel(lsvcModel, SQM_MUC, PRICE_MUC);
+		predictModel(lsvcModel, SQM_FN, PRICE_FN);
+
+		spark.stop();
+	}
+
+	private static SparkSession initSparkSession() {
+		SparkSession spark = SparkSession.builder().appName("JavaLinearSVCExample").config("spark.master", "local")
+				.getOrCreate();
+		spark.sparkContext().setLogLevel("ERROR");
+		return spark;
+	}
+
+	private static LinearSVCModel getSVCModel(Dataset<Row> training) {
+		// Set parameter for Support Vector Machine
+		LinearSVC lsvc = new LinearSVC().setMaxIter(10).setRegParam(0.1);
 		// Fit the model
 		LinearSVCModel lsvcModel = lsvc.fit(training);
 
 		// Print the coefficients and intercept for LinearSVC
 		System.out.println("Coefficients: " + lsvcModel.coefficients() + " Intercept: " + lsvcModel.intercept());
-
-		Vector testData = Vectors.dense(2100, 102);
-		System.out.println("Prediction: " + lsvcModel.predict(testData));
-
-		spark.stop();
+		return lsvcModel;
 	}
+
+	private static void predictModel(LinearSVCModel lsvcModel, double sqm, double price) {
+		Vector testData = Vectors.dense(price, sqm);
+		System.out.println("Prediction for " + sqm + "m² and " + price + "€: " + lsvcModel.predict(testData));
+	}
+
 }
--- src/main/java/de/doubleslash/ml/techdays/RegressionExample.java
+++ src/main/java/de/doubleslash/ml/techdays/RegressionExample.java
@@ -10,13 +10,12 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
 public class RegressionExample {
+	private static SparkSession spark;
 	private static final double SQM = 73;
 	private static final double ROOMS = 3;
 
 	public static void main(String[] args) {
-		SparkSession spark = SparkSession.builder().appName("LinearRegressionMietkostenFn")
-				.config("spark.master", "local").getOrCreate();
-		spark.sparkContext().setLogLevel("ERROR");
+		initSparkSession();
 
 		// Load training data.
 		Dataset<Row> trainingData = spark.read().format("libsvm").load("data/mietkosten_fn.txt");
@@ -25,9 +24,15 @@ public class RegressionExample {
 
 		printModelData(trainingData, lrModel);
 
-		predictValues(lrModel);
+		predictValues(lrModel, SQM, ROOMS);
 
 		spark.stop();
+	}
+
+	private static void initSparkSession() {
+		spark = SparkSession.builder().appName("Java Spark SQL basic example").config("spark.master", "local")
+				.getOrCreate();
+		spark.sparkContext().setLogLevel("ERROR");
 	}
 
 	private static LinearRegressionModel getTrainedModel(SparkSession spark, Dataset<Row> trainingData) {
@@ -49,13 +54,12 @@ public class RegressionExample {
 
 		// Summarize the model over the training set and print out some metrics.
 		LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
-		trainingSummary.residuals().show(false);
 		System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
 		System.out.println("r2: " + trainingSummary.r2());
 	}
 
-	private static void predictValues(LinearRegressionModel lrModel) {
-		Vector testData = Vectors.dense(SQM, ROOMS);
-		System.out.println("Prediction: " + lrModel.predict(testData));
+	private static void predictValues(LinearRegressionModel lrModel, double sqm, double rooms) {
+		Vector testData = Vectors.dense(sqm, rooms);
+		System.out.println("Prediction for " + sqm + "m² and " + rooms + " rooms: " + lrModel.predict(testData));
 	}
 }
